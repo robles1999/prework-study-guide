cbeafa8b0c30369b20f59b57d60ce6bf527ec6bd
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-01-11 15:49:01 -0500

    Added functions

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-// if (topic === "HTML") {
-//   console.log("Let's study HTML");
-// } else if (topic === "CSS") {
-//   console.log("Let's study CSS!");
-// } else if (topic === "Git") {
-//   console.log("Let's study Git!");
-// } else if (topic === "JavaScript") {
-//   console.log("Let's study JavaScript!");
-// } else {
-//   console.log("Please try again!");
-// }
+//::::: FUNCTION SECTION :::::
 
-// topics.map(i => console.log(i))
-for (let i=0; i < topics.length; i++)
-{
-    console.log(topics[i])
+function listTopics() {
+  topics.map((i) => console.log(i));
+  //   for (let i = 0; i < topics.length; i++) {
+  //     console.log(topics[i]);
+  //   }
 }
+
+function selectTopic() {
+  if (randomTopic === "HTML") {
+    console.log("Let's study HTML");
+  } else if (randomTopic === "CSS") {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === "Git") {
+    console.log("Let's study Git!");
+  } else if (randomTopic === "JavaScript") {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log("Please try again!");
+  }
+}
+
+//::::: MAIN CODE :::::
+
+console.log("Here are the topics we learned through Prework: ");
+listTopics();
+console.log("Which topic should we learn first?");
+selectTopic();
